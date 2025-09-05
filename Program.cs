d60d3b79b0a32bcadd2062362ac587f016e68c98
Daniel Kindl <daniel.kindl55@gmial.com>
2025-09-05 16:21:54 +0200

    added .gitignore file

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace directory_sync
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Directory Sync Tool";
+
+
+            Console.ReadKey();
+        }
+    }
+}
